137af27fb4b04ee4913718b180de21ddae555417
Hitoshi Yamamoto <60470969+hitoshyamamoto@users.noreply.github.com>
2020-05-22 22:56:33 -0300

    Solução 1052

---
+++ 1052-mes.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int entrada;
+    scanf("%d",&entrada);
+
+    if(entrada==1) printf("January\n");
+    else if(entrada==2) printf("February\n");
+    else if(entrada==3) printf("March\n");
+    else if(entrada==4) printf("April\n");
+    else if(entrada==5) printf("May\n");
+    else if(entrada==6) printf("June\n");
+    else if(entrada==7) printf("July\n");
+    else if(entrada==8) printf("August\n");
+    else if(entrada==9) printf("September\n");
+    else if(entrada==10) printf("October\n");
+    else if(entrada==11) printf("November\n");
+    else if(entrada==12) printf("December\n");
+
+    return 0;
+}
