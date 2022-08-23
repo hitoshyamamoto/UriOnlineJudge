7ac487fa18ba9cce646ace6cf60b196cff3c8558
Hitoshi Yamamoto <60470969+hitoshyamamoto@users.noreply.github.com>
2022-08-22 21:24:24 -0300

    Solution 1099

---
+++ 1099-SomadeImparesConsecutivos.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int n;
+
+    cin>>n;
+
+    for(int i=0; i<n; i++){
+        int a, b, soma=0;
+        cin>>a>>b;
+
+        if(a<b) for(int j=a+1; j<b; j++){
+            if(j%2!=0) soma+=j;
+        }
+        else for(int j=b+1; j<a; j++){
+            if(j%2!=0) soma+=j;
+        }
+        cout<<soma<<endl;
+
+    }
+
+    return 0;
+}
