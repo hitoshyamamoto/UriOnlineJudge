0fce05990cbb5a5de9df4a0884ee24cbceef7a00
Hitoshi Yamamoto <60470969+hitoshyamamoto@users.noreply.github.com>
2022-08-30 05:26:49 -0300

    [Resolved] 2987

---
+++ 2987-BalaoHonra.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    char entrada;
+    int resposta;
+
+    scanf("%c",&entrada);
+
+    resposta = entrada - 64;
+    cout << resposta << endl;
+
+    return 0;
+}
