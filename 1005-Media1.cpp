153f6487b5251e3d8d60e3f7e5bd0409735aea58
Hitoshi Yamamoto <60470969+hitoshyamamoto@users.noreply.github.com>
2022-09-06 11:24:50 -0300

    [Resolved] 1005

---
+++ 1005-Media1.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    double a, b;
+    cin >> a >> b;
+
+    double media = (a*3.5+b*7.5)/11;
+    cout << "MEDIA = " << fixed << setprecision(5) << media << endl;
+
+    return 0;
+}
