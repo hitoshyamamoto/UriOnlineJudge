6527350cd15bf8fef7c20e846c8336509b6db0b5
Hitoshi Yamamoto <60470969+hitoshyamamoto@users.noreply.github.com>
2022-09-06 11:13:17 -0300

    [Resolved] 1004

---
+++ 1004-ProdutoSimples.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int a, b;
+    cin >> a >> b;
+
+    int produto;
+    produto = a*b;
+
+    cout << "PROD = " << produto << endl;
+
+    return 0;
+}
