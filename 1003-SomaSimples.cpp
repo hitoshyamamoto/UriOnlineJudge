f3110a12ff6b109ce6e44ce05c65eba23b81e67d
Hitoshi Yamamoto <60470969+hitoshyamamoto@users.noreply.github.com>
2022-09-06 10:29:50 -0300

    [Resolved] 1003

---
+++ 1003-SomaSimples.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int a, b;
+    cin >> a >> b;
+
+    int resposta=a+b;
+    cout << "SOMA = " << resposta << endl;
+
+    return 0;
+}
