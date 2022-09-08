1b25838807d2ce6978637ad1565d102cb9b110e6
Hitoshi Yamamoto <60470969+hitoshyamamoto@users.noreply.github.com>
2022-09-08 13:56:52 -0300

    [Resolved] 1007

---
+++ 1007-Diferenca.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+    int a, b, c, d;
+    cin >> a >> b >> c >> d;
+
+    int resultado;
+    resultado = a*b-c*d;
+
+    cout << "DIFERENCA = " << resultado << endl;
+
+    return 0;
+}
